1ecafefef9b436c8c8e09ccd7b7cdfbb9bad017b
Lucas dos Santos <santoslucasantos796@gmail.com>
2022-07-29 16:35:16 -0300

    Rodapé global das paginas

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+
+    return (
+        <footer className="container">
+            <div className="float-end"><a href="#"><button className="topo btn btn-outline-success">Voltar ao topo</button></a></div>
+            <div style={{textAlign:"center"}}>2022 © FRM, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></div>
+            </footer>
+    )
+}
