adfaf512cc714b190c7ee2c3bbea14a4c365c35d
Lucas dos Santos <santoslucasantos796@gmail.com>
2022-07-29 16:17:43 -0300

    #10 Criação da pagina de exibição de buscas

---
+++ src/pages/search.tsx
@@ -0,0 +1,58 @@
+import type { NextPage } from 'next'
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Sidebar from '../components/Sidebar'
+
+const Search: NextPage = () => {
+    return (
+        <>
+            <body>
+                <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
+                    data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+                    <Header />
+                    <Sidebar />
+                    <div className="page-wrapper">
+                        <div className="page-breadcrumb bg-white" >
+                            <div className="row align-items-center">
+                                <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                                    <h4 className="page-title">Resultados da pesquisa para </h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container-fluid">
+                            <div className="row justify-content-center">
+                                <div className="row">
+                                    <div className="col-md-12 col-lg-12 col-sm-12">
+                                        <div className="white-box">
+                                            <div className="table-responsive">
+                                                <table className="table no-wrap">
+                                                    <thead>
+                                                        <tr>
+                                                            <th className="border-top-0">#</th>
+                                                            <th className="border-top-0">Medicamento</th>
+                                                            <th className="border-top-0">Quantidade</th>
+                                                            <th className="border-top-0">Comprimido ou gotas</th>
+                                                            <th className="border-top-0">Situação</th>
+                                                            <th className="border-top-0">Ações</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr id="div" className="featurette-divider" />
+                <Footer />
+            </body >
+        </>
+    )
+}
+
+export default Search
